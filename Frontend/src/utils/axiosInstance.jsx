0ea77e0d2c90e0f0b661861fffe3b98a8ace1027
sudheeesh <sudheeshvilla5@gmail.com>
2025-08-02 11:26:07 +0530

    new changes

--- Frontend/src/utils/axiosInstance.jsx
+++ Frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api/v1', // Vite proxy will forward this to backend
+  baseURL: 'https://mern-ecommerce-gxhk.onrender.com/api/v1', // Vite proxy will forward this to backend
   withCredentials: true, // needed if using cookies/JWT
 });
 
