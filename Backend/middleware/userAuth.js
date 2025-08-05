3c05620aec1c7cae84f4b926ee46107b68d9754e
sudheeesh <sudheeshvilla5@gmail.com>
2025-08-05 20:11:45 +0530

    user

--- Backend/middleware/userAuth.js
+++ Backend/middleware/userAuth.js
@@ -4,19 +4,8 @@ import jwt from "jsonwebtoken"
 import User from "../models/userModel.js"
 
 export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
-  let token;
-
-  // Check cookie token first
-  if (req.cookies && req.cookies.token) {
-    token = req.cookies.token;
-  }
-  // Fallback to Authorization header
-  else if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer ")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+ 
+    const {token} = req.cookies
 
   // If token is still not found
   if (!token) {
