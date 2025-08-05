9d486e2981439c9697d3b79523d937ec5141fd6c
sudheeesh <sudheeshvilla5@gmail.com>
2025-08-05 17:00:35 +0530

    userauth

--- Backend/middleware/userAuth.js
+++ Backend/middleware/userAuth.js
@@ -1,23 +1,33 @@
 import HandleError from "../utils/handleError.js";
 import handleAsyncError from "./handleAsyncError.js";
-import jwt from "jsonwebtoken"
-import User from "../models/userModel.js"
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
-export const verifyUserAuth =handleAsyncError(async(req,res,next)=>{
-    const {token} = req.cookies;
-    if(!token){
-        return next(new HandleError("Authentication is missing! please login to access the  resource",401))
-    }
-    const decodedData = jwt.verify(token,process.env.JWT_SECRET_KEY)
-    req.user=await User.findById(decodedData.id)
-    next()  
-})
+export const verifyUserAuth = handleAsyncError(async (req, res, next) => {
+  let token;
 
-export const roleBasedAccess=(...roles) =>{
-    return(req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-            return next(new HandleError(`Role - ${req.user.role} is not allowed to access the resource`,403))
-        }
-        next()
-    }
-}
+  // Check cookie token first
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+  // Fallback to Authorization header
+  else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  // If token is still not found
+  if (!token) {
+    return next(
+      new HandleError("Authentication is missing! Please login to access the resource", 401)
+    );
+  }
+
+  // Decode token
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await User.findById(decodedData.id);
+
+  next();
+});
