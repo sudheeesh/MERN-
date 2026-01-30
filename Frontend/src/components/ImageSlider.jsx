09486536c2d86e2d1257eedc4c21d9743760248d
sudheeesh <sudheeshvilla5@gmail.com>
2026-01-30 15:33:39 +0530

    caraousel

--- Frontend/src/components/ImageSlider.jsx
+++ Frontend/src/components/ImageSlider.jsx
@@ -21,7 +21,7 @@ const ImageSlider = () => {
 
   }, [])
   return (
-    <div className="relative w-full h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden mt-20 md:mt-24">
+    <div className="relative w-full aspect-square sm:aspect-video md:h-[400px] lg:h-[500px] overflow-hidden mt-28 md:mt-24">
       <div
         className="flex transition-transform duration-1000 ease-in-out h-full"
         style={{ transform: `translateX(-${currentIndex * 100}%)` }}
